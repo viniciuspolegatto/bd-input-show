af53e061a0ab49317f861b111e499ac2b0a2bc6c
Glitch (glitch-hello-node) <none>
2024-07-19 14:55:17 +0000

    🍐🐾 Checkpoint ./server.js:38290350/994

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-let endpoint1 = 'https://receitaws.com.br/v1/cnpj/29735567000198';
-let endpoint = 'https://raw.githubusercontent.com/guilhermeonrails/api-frontend/main/produtos.json'
+const http = require('https');
 
-async function buscarCnpjAPI() {
-  try {
-    let res = await fetch(endpoint);  // Aguarda a resposta da requisição
-    let data = await res.json();      // Extrai o JSON da resposta
-
-    console.log(data);                // Faz algo com os dados recebidos
-  } catch (error) {
-    console.error('Erro ao buscar CNPJ:', error);
+const options = {
+  method: 'GET',
+  hostname: 'receitaws.com.br',
+  port: null,
+  path: '/v1/cnpj/29735567000198',
+  headers: {
+    Accept: 'application/json'
   }
-}
+};
 
-buscarCnpjAPI();
+const req = http.request(options, function (res) {
+  const chunks = [];
+
+  res.on('data', function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on('end', function () {
+    const body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
